a38ebb44eb28c9371d3c4922332f08fe8d78adf6
lawrenceyim <lawrence.yim.010@gmail.com>
2024-01-16 17:17:18 -0500

    Added SQL database generation script

---
+++ src/main/resources/GenerateDatabase.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Subjects (
+  id INT NOT NULL,
+  name VARCHAR(40) NOT NULL,
+  room INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Students (
+  id INT NOT NULL,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  group_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE StudentGroups (
+  id INT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE Schedules (
+  group_id INT NOT NULL,
+  subject_id INT NOT NULL,
+  time_slot INT NOT NULL,
+  day_of_week VARCHAR(20) NOT NULL,
+  FOREIGN KEY (group_id) REFERENCES StudentGroups(id),
+  FOREIGN KEY(subject_id) REFERENCES Subjects(id)
+);
