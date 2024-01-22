74eedaa47aa5f51468834e442878b46f819b68b7
lawrenceyim <lawrence.yim.010@gmail.com>
2024-01-22 18:59:52 -0500

    Changed linkedhastset to list for element specific access

--- src/main/java/com/solvd/schedulegenerator/service/impl/ScheduleGenerationServiceImpl.java
+++ src/main/java/com/solvd/schedulegenerator/service/impl/ScheduleGenerationServiceImpl.java
@@ -6,16 +6,19 @@ import com.solvd.schedulegenerator.domain.Subject;
 import com.solvd.schedulegenerator.service.ScheduleGenerationService;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class ScheduleGenerationServiceImpl implements ScheduleGenerationService {
     private List<StudentGroup> groups;
-    private List<Subject> subjects;
-    private List<Course> coursesWithConstraint;
-    private int coursesPerDay;          // No more than 5 per day
+    private List<Subject> subjects;  // All subjects
+    private List<Subject> subjectsWithConstraint;  // Subjects with constraints
+    private int coursesPerDay;  // No more than 5 per day
     private final int DAYS_PER_WEEK = 5;
-    private Subject[][][] schedule;     // [group][day of the week][timeslot]
+    private Subject[][][] schedule;  // [group][day of the week][timeslot]
+    private List<Subject> subjectsToAdd;  // Iterate over this linked hashset to add courses to schedule
     private int subjectIterator = 0;
     private int coursesAdded = 0;
     private int totalCourses;
@@ -24,41 +27,49 @@ public class ScheduleGenerationServiceImpl implements ScheduleGenerationService 
     }
 
     public static class Builder {
-        private final ScheduleGenerationServiceImpl scheduleGenerationService;
+        private final ScheduleGenerationServiceImpl service;
 
         public Builder() {
-            this.scheduleGenerationService = new ScheduleGenerationServiceImpl();
+            this.service = new ScheduleGenerationServiceImpl();
         }
 
         public Builder setGroups(List<StudentGroup> groups) {
-            scheduleGenerationService.groups = groups;
+            service.groups = groups;
             return this;
         }
 
         public Builder setSubjects(List<Subject> subjects) {
-            scheduleGenerationService.subjects = subjects;
+            service.subjects = subjects;
             return this;
         }
 
-        public Builder setCoursesWithConstraints(List<Course> coursesWithConstraints) {
-            scheduleGenerationService.coursesWithConstraint = coursesWithConstraints;
+        public Builder setSubjectsWithConstraints(List<Subject> subjectsWithConstraint) {
+            service.subjectsWithConstraint = subjectsWithConstraint;
             return this;
         }
 
         public Builder setCoursesPerDay(int coursesPerDay) {
-            scheduleGenerationService.coursesPerDay = coursesPerDay;
-            scheduleGenerationService.schedule = new Subject
-                    [scheduleGenerationService.groups.size()]
-                    [scheduleGenerationService.DAYS_PER_WEEK]
-                    [scheduleGenerationService.coursesPerDay];
-            scheduleGenerationService.totalCourses =
-                    scheduleGenerationService.coursesPerDay *
-                            scheduleGenerationService.DAYS_PER_WEEK;
+            service.coursesPerDay = coursesPerDay;
+            service.schedule = new Subject[service.groups.size()][service.DAYS_PER_WEEK][service.coursesPerDay];
+            service.totalCourses = service.coursesPerDay * service.DAYS_PER_WEEK;
+            service.subjectsToAdd = sortSubjectsByPriority(service.subjectsWithConstraint, service.subjects);
             return this;
         }
 
         public ScheduleGenerationService build() {
-            return scheduleGenerationService;
+            return service;
+        }
+
+        // Class with constraints go first. Then classes without constraints
+        private List<Subject> sortSubjectsByPriority(List<Subject> subjectsWithConstraint, List<Subject> subjectsWithoutConstraint) {
+            LinkedHashSet<Subject> orderedSubjects = new LinkedHashSet<>();
+            subjectsWithConstraint.stream().forEach(subject -> {
+                orderedSubjects.add(subject);
+            });
+            subjectsWithoutConstraint.stream().forEach(subject -> {
+                orderedSubjects.add(subject);
+            });
+            return new LinkedList<>(orderedSubjects);
         }
     }
 
@@ -81,19 +92,19 @@ public class ScheduleGenerationServiceImpl implements ScheduleGenerationService 
         for (int day = 0; day < DAYS_PER_WEEK; day++) {
             for (int timeslot = 0; timeslot < coursesPerDay; timeslot++) {
                 if (schedule[group][day][timeslot] == null) {
-                    schedule[group][day][timeslot] = subjects.get(subjectIterator);
+                    schedule[group][day][timeslot] = subjectsToAdd.get(subjectIterator);
                     if (hasNoConflict(group, day, timeslot)) {
                         group++;
                         if (group == groups.size()) {
                             coursesAdded++;
                             group = 0;
-                            subjectIterator = (subjectIterator + 1) % subjects.size();
+                            subjectIterator = (subjectIterator + 1) % subjectsToAdd.size();
                         }
                         // If solution is invalid, back track
                         if (!depthFirstSearch(group)) {
                             subjectIterator--;
                             if (subjectIterator < 0) {
-                                subjectIterator = subjects.size() - 1;
+                                subjectIterator = subjectsToAdd.size() - 1;
                             }
                             group--;
                             if (group < 0) {
