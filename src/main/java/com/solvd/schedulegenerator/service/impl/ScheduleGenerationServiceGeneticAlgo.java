1280d11ebaa12b7017eb043799523beab8d4e3e4
lawrenceyim <lawrence.yim.010@gmail.com>
2024-01-27 05:39:27 -0500

    Added skeleton outline for the class

--- src/main/java/com/solvd/schedulegenerator/service/impl/ScheduleGenerationServiceGeneticAlgo.java
+++ src/main/java/com/solvd/schedulegenerator/service/impl/ScheduleGenerationServiceGeneticAlgo.java
@@ -4,6 +4,7 @@ import com.solvd.schedulegenerator.domain.Course;
 import com.solvd.schedulegenerator.domain.StudentGroup;
 import com.solvd.schedulegenerator.domain.Subject;
 import com.solvd.schedulegenerator.service.ScheduleGenerationService;
+import com.solvd.schedulegenerator.utils.Pair;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,10 +13,9 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ScheduleGenerationServiceGeneticAlgo implements ScheduleGenerationService {
-    // Each element represents timeslot. Grouped by student group and day. Use % to find specific class
-    private Long[] schedule; // Subjects represented by their id
     private List<StudentGroup> groups;
     private List<Subject> subjects;  // All subjects
     private List<Subject> subjectsWithConstraint;  // Subjects with constraints
@@ -27,6 +27,8 @@ public class ScheduleGenerationServiceGeneticAlgo implements ScheduleGenerationS
     private int[] coursesAddedToGroup;
     private HashMap<Long, Subject> subjectIdMap;
     private HashSet<List<Long>> tabuList; // Similar to Tabu search, maintain a list of previously seen solutions
+    // Each element represents timeslot. Grouped by student group and day. Use % to find specific class
+    private List<List<Long>> schedules;
 
     private ScheduleGenerationServiceGeneticAlgo() {
     }
@@ -56,12 +58,12 @@ public class ScheduleGenerationServiceGeneticAlgo implements ScheduleGenerationS
         public ScheduleGenerationServiceGeneticAlgo.Builder setCoursesPerDay(int coursesPerDay) {
             service.coursesPerDay = coursesPerDay;
             service.totalCourses = service.coursesPerDay * service.DAYS_PER_WEEK;
-            service.schedule = new Long[service.totalCourses * service.groups.size()];
             service.numberOfRooms = service.subjects.size();
             service.subjectsToAdd = sortSubjectsByPriority();
             service.coursesAddedToGroup = new int[service.groups.size()];
             service.subjectIdMap = generateSubjectIdMap();
             service.tabuList = new HashSet<>();
+            service.schedules = new ArrayList<>();
             return this;
         }
 
@@ -92,6 +94,15 @@ public class ScheduleGenerationServiceGeneticAlgo implements ScheduleGenerationS
 
     @Override
     public List<Course> generateSchedule() {
+        IntStream.range(0, 1000).forEach(i -> {
+            // TODO: Initialize population
+            // TODO: Evaluate fitness
+            // TODO: Select parents
+            // TODO: Create offsprings (recombine)
+            // TODO: Create mutations
+            // TODO: Create new population
+        });
+        // TODO: Find and return schedule with the highest fittness score
         return null;
     }
 
@@ -100,17 +111,19 @@ public class ScheduleGenerationServiceGeneticAlgo implements ScheduleGenerationS
         return group * subjects.size() + timeslot;
     }
 
-    private Long findSubjectByGroupAndTimeslot(int group, int timeslot) {
-        return schedule[findIndexByGroupAndTimeslot(group, timeslot)];
+    private Long findSubjectByGroupAndTimeslot(int group, int timeslot, int index) {
+        return schedules.get(index).get(findIndexByGroupAndTimeslot(group, timeslot));
     }
 
-    private void setSubjectByGroupAndTimeslot(int group, int timeslot, Long subjectId) {
-        schedule[findIndexByGroupAndTimeslot(group, timeslot)] = subjectId;
+    private void setSubjectByGroupAndTimeslot(int group, int timeslot, Long subjectId, int index) {
+        List<Long> schedule = schedules.get(index);
+        schedule.set(findIndexByGroupAndTimeslot(group, index), subjectId);
+        schedules.set(index, schedule);
     }
 
-    private List<Subject> convertIdToSubject() {
+    private List<Subject> convertIdToSubject(List<Long> schedule) {
         List<Subject> list = new LinkedList<>();
-        Arrays.stream(schedule).forEach(id -> {
+        schedule.forEach(id -> {
             list.add(subjectIdMap.get(id));
         });
         return list;
@@ -122,5 +135,47 @@ public class ScheduleGenerationServiceGeneticAlgo implements ScheduleGenerationS
 
     private boolean isInTabuList(Long[] state) {
         return tabuList.contains(state);
+    }
+
+    // TODO: Determine population size
+    private void generateRandomSchedules() {
+        IntStream.range(0, 1000).forEach(i -> {
+            schedules.add(generateRandomSchedule());
+        });
+    }
+
+    private List<Long> generateRandomSchedule() {
+        ArrayList<Long> schedule = new ArrayList<>(totalCourses * groups.size());
+        // TODO: Implement code to generate random schedules
+        return schedule;
+    }
+
+    // TODO: Add code to evaluate fitness of individuals in the population
+    private int evaluateFitness(int index) {
+        int fitness = 0;
+        return fitness;
+    }
+
+    // TODO: Add code to select pairs of elements in the population to serve as parents for new offsprings
+    private List<Pair<Integer, Integer>> selection() {
+        ArrayList<Pair<Integer, Integer>> parents = new ArrayList<>();
+        return parents;
+    }
+
+    // TODO: Add code to recombine parents into offsprings
+    private List<List<Long>> recombination(List<Pair<Integer, Integer>> parents) {
+        List<List<Long>> offsprings = new ArrayList<>();
+        return offsprings;
+    }
+
+    // TODO: Add code to mutate individuals in the population
+    private List<List<Long>> mutate() {
+        List<List<Long>> mutations = new ArrayList<>();
+        return mutations;
+    }
+
+    // TODO: Add code to create a new population for the next generation
+    private void replace(List<List<Long>> parents, List<List<Long>> mutations) {
+
     }
 }
